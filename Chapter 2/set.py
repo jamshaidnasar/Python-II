7a97c17c3badb03299b5713173f336d8b195ad15
Jamshaid Nasar <Jamshaidnasar829@gmail.com>
2025-09-05 21:38:54 +0200

    new

---
+++ Chapter 2/set.py
@@ -0,0 +1,10 @@
+basket = { 'apple', 'orange', 'apple', 'pear', 'orange', 'banana' }
+
+# duplicates will be removed
+print(basket)
+
+alphabets = set("qwertzuiopasdfghjklyxcvbnm")
+print(alphabets)
+
+
+cities = {}
---
+++ Chapter 2/string.py
@@ -0,0 +1,23 @@
+name = "JamshaidNasar"
+
+# print full name
+print(name)
+
+# print from 0 - 8 then 8 - 13
+print("First Name:", name[0:8])
+print("Last Name:", name[8:13])
+
+# find letter position
+print(name.find("J"))
+print(name.find("a"))
+print(name.find("m"))
+print(name.find("s"))
+print(name.find("h"))
+print(name.find("a"))
+print(name.find("i"))
+print(name.find("d"))
+print(name.find("N"))
+print(name.find("a"))
+print(name.find("s"))
+print(name.find("a"))
+print(name.find("r"))
